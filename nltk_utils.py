984a9c0bdac237a16a37920d99c1cff5e09aa8e3
Aditya Shukla <adityashukla2015@gmail.com>
2020-11-21 21:29:29 -0500

    Create nltk_utils.py

---
+++ nltk_utils.py
@@ -0,0 +1,49 @@
+import numpy as np
+import nltk
+
+from nltk.stem.porter import PorterStemmer
+stemmer = PorterStemmer()
+
+# json comments
+# This json file will be accesable to the etirety of my program. The variables 
+# I have labeled below as "intentions", will be my training data set for the 
+# Feed Forward Neural Network. I have classified the different types of intentions as tags,
+# i. e. gretings, goodbyes, etc. 
+
+def tokenize(sentence):
+    """
+    split sentence into array of words/tokens
+    a token can be a word or punctuation character, or number
+    """
+    return nltk.word_tokenize(sentence)
+
+
+def stem(word):
+    """
+    stemming = find the root form of the word
+    examples:
+    words = ["organize", "organizes", "organizing"]
+    words = [stem(w) for w in words]
+    -> ["organ", "organ", "organ"]
+    """
+    return stemmer.stem(word.lower())
+
+
+def bag_of_words(tokenized_sentence, words):
+    """
+    return bag of words array:
+    1 for each known word that exists in the sentence, 0 otherwise
+    example:
+    sentence = ["hello", "how", "are", "you"]
+    words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+    bog   = [  0 ,    1 ,    0 ,   1 ,    0 ,    0 ,      0]
+    """
+    # stem each word
+    sentence_words = [stem(word) for word in tokenized_sentence]
+    # initialize bag with 0 for each word
+    bag = np.zeros(len(words), dtype=np.float32)
+    for idx, w in enumerate(words):
+        if w in sentence_words: 
+            bag[idx] = 1
+
+    return bag
